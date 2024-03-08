0ac7b43e384b6e0dce3699453132748a499711ce
Naveen Noorbhasha <naveen.noorbhasha@raisin.com>
2024-03-08 09:03:10 +0100

    Add More

--- Terraform-Core-Concepts/first-aws-ec2/first-ec2.tf
+++ Terraform-Core-Concepts/first-aws-ec2/first-ec2.tf
@@ -1,14 +1,15 @@
 provider "aws" {
   region     = "eu-central-1"
-  access_key = "XXXXXXXXXXXXXXXXXXXX"
-  secret_key = "XXXXXXXXXXXXXXXXXXXX"
+  shared_config_files = ["/Users/naveen.noor/Desktop/my-github-repos/Terraform/Terraform-Core-Concepts/.aws/conf"]
+  shared_credentials_files = ["/Users/naveen.noor/Desktop/my-github-repos/Terraform/Terraform-Core-Concepts/.aws/creds"]
+  profile = "default"
 }
 
 resource "aws_instance" "first-ec2" {
   ami           = "ami-0a23a9827c6dab833"
   instance_type = "t2.micro"
   tags          = {
-    Name = "first-tf-instance"
+    Name = "tf-instance"
   }
 }
 /*
